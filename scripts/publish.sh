609c51107bc42ad65003753fb5d9d6d6be12da23
HRDVV <hrd.candy.dev@gmail.com>
2019-11-15 18:28:46 +0800

    增加发布脚本

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,1 +1,20 @@
 #! usr/bin/env sh
+
+set -e;
+read -p "请输入发布版本: " VERSION;
+read -p "确定要发布 $VERSION 版本吗？[y/n]" -n 1;
+
+if [ $REPLY == "y" ] 
+then
+  echo "\n"
+  read -p "是否npm test ? [y/n]" -n 1;
+  if [ $REPLY == "y" ]
+  then
+  npm test
+  fi
+  git add .
+  git commit -m '发布 $VERSION'
+  npm version $VERSION -m "发布 $VERSION" --allow-same-version;
+  npm publish
+fi
+

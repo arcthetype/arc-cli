ca6eb24dc37688d84e0ec500205ec86d3cfd3639
HRDVV <hrd.candy.dev@gmail.com>
2019-11-16 11:43:14 +0800

    修改脚本

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -e;
 read -p "请输入发布版本: " VERSION;
 read -p "确定要发布 $VERSION 版本吗？[y/n]" -n 1 -r;
 
-if [ $REPLY =~ ^[Yy]$ ] 
+if [[ $REPLY =~ ^[Yy]$ ]] 
 then
   npm version $VERSION -m "发布 $VERSION" --allow-same-version;
   npm publish
